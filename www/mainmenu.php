7ee6b3acd7b29f7c5a596fdf99b0a4923966a93e
italianindy <chayutwork@gmail.com>
2016-08-20 16:38:23 +0700

    Version 1.0.0
    Upload Version 1.0.0

---
+++ www/mainmenu.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html> 
+<html>
+<head>
+<meta charset="utf-8">
+<title>TOUCH SERVICE</title>
+<link href="jquery-mobile/jquery.mobile.theme-1.0.min.css" rel="stylesheet" type="text/css"/>
+<link href="jquery-mobile/jquery.mobile.structure-1.0.min.css" rel="stylesheet" type="text/css"/>
+<script src="jquery-mobile/jquery-1.6.4.min.js" type="text/javascript"></script>
+<script src="jquery-mobile/jquery.mobile-1.0.min.js" type="text/javascript"></script>
+</head> 
+<body> 
+
+<div data-role="page" data-theme="b" id="pageMainmenu">
+	<div data-role="header" data-theme="d">
+		<h1><strong>TOUCH SERVICE</strong></h1>
+	</div>
+	<div data-role="content" >
+    <div align="center">
+      <img src="pic/menu.png" width="480" height="120"> </div>
+       <font size="4"> <ul data-inset="true" data-role="listview" data-theme="d">
+		  <li>
+      <a href="http://119.59.125.117/thaitimeemployee/popuplogin.php" data-rel="dialog" data-transition="pop"><p> <img src="pic/school.png" width="250" height="50"><p></a></li>
+            <li><a href="http://119.59.125.117/thaitimeemployee/popuplogin.php" data-rel="dialog" data-transition="pop"><p> <img src="pic/employee.png" width="250" height="50"><p></a></li>
+			<li><a href="http://119.59.125.117/thaitimeemployee/popuplogin.php" data-rel="dialog" data-transition="pop"><p> <img src="pic/car.png" width="250" height="50"><p></a></li>
+		</ul></font>	
+        
+      
+        	
+	</div>
+	<div data-role="footer" data-theme="d" data-position="fixed">
+		<div data-role="navbar">
+          <ul>
+            <li><a href="http://119.59.125.117/thaitimeemployee/mainmenu.php"><font size="4">ติดต่อเรา</font></a></li>
+            <li><a href="#"><font size="4">แนะนำการใช้งาน</font></a></li>
+          </ul>
+        </div>
+  </div>
+</div>
+
+
+
+
+ </body>
+</html>
